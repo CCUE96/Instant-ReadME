94b3823bf395c2e471d376c6935a6471196aa523
Christopher Cuellar <rival100x@gmail.com>
2024-04-09 01:12:28 -0400

    created dynamic readme added if statements for license badge and url

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,81 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is the title of your repository?",
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Write a short description of your project",
+    },
+    {
+        type: "input",
+        name: "userstory",
+        message: "Write A User Story?",
+    },
+
+    {
+        type: "checkbox",
+        name: "Languages",
+        message: "Which coding languages were used in this project?",
+        choices:["html","CSS","javascript","node.js","java","python","C++","C#","C"
+        ]
+    },
+
+    {
+        type: "input",
+        name: "username",
+        message: "Enter your GitHub username",
+    },
+  
+    {
+        type: "input",
+        name: "installation",
+        message: "What are the steps required to install your project?",
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "What is the main use of your project?",
+    },
+    {
+        type: "input",  
+        name: "contributors",
+        message: "Who collaborated with you on this project? if None Press enter to skip",
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "Select a license for your project",
+        choices: ["MIT", "Apache", "GPL", "BSD", "None"],
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(data) {
+    fs.writeFile("README.md", data, (err) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("Readme file created successfully");
+        }
+    });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((answers) => {
+        const markdown = generateMarkdown(answers);
+        writeToFile(markdown);
+    });
+}
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,18 +1,97 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license === "MIT")
+  return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)[(https://opensource.org/licenses/MIT)]`
+else if (license === "Apache")
+  return `[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
+else if (license === "GPL")
+  return `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://opensource.org/licenses/GPL-3.0)`
+else if (license === "BSD")
+  return `[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)`
+else if (license === "None")
+  return ''
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === "MIT")
+  return `https://opensource.org/licenses/MIT`
+else if (license === "Apache")
+  return `https://opensource.org/licenses/Apache-2.0`
+else if (license === "GPL")
+  return `https://opensource.org/licenses/GPL-3.0`
+else if (license === "BSD")
+  return `https://opensource.org/licenses/BSD-3-Clause`
+else if (license === "None")
+  return ''
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license === "MIT")
+  return `MIT License`
+else if (license === "Apache")
+  return `Apache License 2.0`
+else if (license === "GPL")
+  return `GPL 3.0`
+else if (license === "BSD")
+  return `BSD 3-Clause`
+else if (license === "None")
+  return ''
+}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+function generateMarkdown(data ) {
+  return `# <ins>${data.title}</ins>
+  
+  ## <ins>Description</ins>
+  ${data.description}
+
+  ## <ins>User Story</ins> 
+  ${data.userstory}
+  
+  Table Of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+
+  ## <ins>Github</ins>
+  https://github.com/${data.username}
+
+ 
+
+  ## <ins>Installation</ins>
+
+  ${data.installation}
+
+  ## <ins>Usage</ins>
+  ${data.usage}
+
+
+  ## <ins>Languages</ins>
+  ${data.Languages}
+
+  ## <ins>Contributors</ins>
+  ${data.contributors}
+
+  ## License
+  ${renderLicenseBadge(data.license)}
+  ${renderLicenseSection(data.license)}
+  ${renderLicenseLink(data.license)}
+
+  ## <ins>Repository</ins>
+  https://github.com/${data.username}/${data.title}
+
+
+  ## <ins>Deployed Application</ins>
+  https://${data.username}.github.io/${data.title}/
+
+
+
+    
 
 `;
 }
